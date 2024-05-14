ddbbbbccf6eccb966df2b56ea48324337eec0e1c
HuyLocDz <161593412+HuyLocDz@users.noreply.github.com>
2024-05-14 15:45:43 +0700

    Update OrionUi.lua

--- OrionUi.lua
+++ OrionUi.lua
@@ -423,7 +423,7 @@ function OrionLib:MakeNotification(NotificationConfig)
 				Size = UDim2.new(1, 0, 0, 0),
 				Position = UDim2.new(0, 0, 0, 25),
 				Font = Enum.Font.GothamSemibold,
-				Name = "Content",
+				Name = "N \ A",
 				AutomaticSize = Enum.AutomaticSize.Y,
 				TextColor3 = Color3.fromRGB(200, 200, 200),
 				TextWrapped = true
@@ -1367,7 +1367,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				})
 
 				local TextboxActual = AddThemeObject(Create("TextBox", {
-					Size = UDim2.new(4, 0, 1, 0),
+					Size = UDim2.new(5, 0, 1, 0),
 					BackgroundTransparency = 1,
 					TextColor3 = Color3.fromRGB(255, 255, 255),
 					PlaceholderColor3 = Color3.fromRGB(210,210,210),
@@ -1381,7 +1381,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				local TextContainer = AddThemeObject(SetChildren(SetProps(MakeElement("RoundFrame", Color3.fromRGB(255, 255, 255), 0, 4), {
 					Size = UDim2.new(0, 24, 0, 24),
 					Position = UDim2.new(1, -12, 0.5, 0),
-					AnchorPoint = Vector2.new(4, 1)
+					AnchorPoint = Vector2.new(5, 2.5)
 				}), {
 					AddThemeObject(MakeElement("Stroke"), "Stroke"),
 					TextboxActual
