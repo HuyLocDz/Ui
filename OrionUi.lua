5540708cf039b7f02a72b3df42baba312cf63cd4
HuyLocDz <161593412+HuyLocDz@users.noreply.github.com>
2024-05-14 18:13:07 +0700

    Update OrionUi.lua

--- OrionUi.lua
+++ OrionUi.lua
@@ -1363,7 +1363,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				TextboxConfig.Callback = TextboxConfig.Callback or function() end
 
 				local Click = SetProps(MakeElement("Button"), {
-					Size = UDim2.new(1, 0, 1, 0)
+					Size = UDim2.new(6, 0, 1, 0)
 				})
 
 				local TextboxActual = AddThemeObject(Create("TextBox", {
@@ -1380,7 +1380,7 @@ function OrionLib:MakeWindow(WindowConfig)
 
 				local TextContainer = AddThemeObject(SetChildren(SetProps(MakeElement("RoundFrame", Color3.fromRGB(255, 255, 255), 0, 4), {
 					Size = UDim2.new(0, 24, 0, 24),
-					Position = UDim2.new(6, 0, 1, 0),
+					Position = UDim2.new(1, -12, 0.5, 0),
 					AnchorPoint = Vector2.new(1, 0.5)
 				}), {
 					AddThemeObject(MakeElement("Stroke"), "Stroke"),
